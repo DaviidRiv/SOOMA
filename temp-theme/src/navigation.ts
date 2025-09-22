6763f35ba93f3c16e6f9d4d98d4cfdc6e775caa9
DaviidRiv <davidmoreno70382@gmail.com>
2025-09-21 23:32:01 -0600

    upd changes and routes

--- temp-theme/src/navigation.ts
+++ temp-theme/src/navigation.ts
@@ -6,10 +6,10 @@ export const headerData = {
     {
       text: 'Quiénes somos',
       links: [
-        { text: 'Misión y Visión', href: getPermalink('#mvv') },
-        { text: '¿Quiénes somos?', href: getPermalink('#nosotros') },
-        { text: 'Contacto',       href: getPermalink('#contacto') },
-        { text: 'Ubicación',      href: getPermalink('#mapa') },
+        { text: 'Misión y Visión', href: getPermalink('/#mvv') },
+        { text: '¿Quiénes somos?', href: getPermalink('/#nosotros') },
+        { text: 'Contacto',       href: getPermalink('/#contacto') },
+        { text: 'Ubicación',      href: getPermalink('/#mapa') },
       ],
     },
 
@@ -91,50 +91,44 @@ export const footerData = {
     {
       title: 'Navegación',
       links: [
-        { text: 'Inicio', href: '/' },
-        { text: 'Marcas', href: '/#brands' },
-        { text: 'Servicios', href: '/#services' },
-        { text: 'Preguntas frecuentes', href: '/#faq' },
-        { text: 'Demostración', href: '/#test-drive' },
-        { text: 'Clientes', href: '/#clients' },
-        { text: 'Nosotros', href: '#nosotros' },
-        { text: 'Contacto', href: '/#contacto' },
+        { text: 'Inicio', href: getPermalink('/') },
+        { text: 'Misión y Visión', href: getPermalink('/#mvv') },
+        //{ text: 'Preguntas frecuentes', href: getPermalink('/#faq') },
+        //{ text: 'Demostración', href: getPermalink('/#test-drive') },
+        //{ text: 'Clientes', href: getPermalink('/#clients') },
+        { text: 'Nosotros', href: getPermalink('/#nosotros') },
+        { text: 'Contacto', href: getPermalink('/#contacto') },
       ],
     },
     {
-      title: 'Política de Cookies',
+      title: 'Políticas de Servicio',
       links: [
-        { text: 'Configuración de cookies', href: '/cookies/configuracion' },
-        { text: 'Más información sobre cookies', href: '/cookies/info' },
-        { text: 'Preferencias de privacidad', href: '/cookies/preferencias' },
+        { text: 'Configuración de cookies', href: '/cookies' },
+        { text: 'Términos y condiciones', href: '/terms' },
+        { text: 'Preferencias de privacidad', href: '/privacy' },
       ],
     },
   ],
 
-  secondaryLinks: [
-    { text: 'Términos', href: '/terms' },
-    { text: 'Privacidad', href: '/privacy' },
+  // Iconos inferiores
+  socialLinks: [
+    {
+      ariaLabel: 'Email',
+      icon: 'tabler:mail',
+      href: 'https://mail.google.com/mail/?view=cm&fs=1&to=emmaidaliasaldana@gmail.com',
+    },
+    { ariaLabel: 'WhatsApp', icon: 'tabler:brand-whatsapp', href: 'https://wa.me/524441425726' },
+    {
+      ariaLabel: 'Mapa',
+      icon: 'tabler:map-pin',
+      href: 'https://www.google.com/maps/search/?api=1&query=Ca%C3%B1ada%20Real%20316%2C%20C.P.%2078215%2C%20Lomas%20del%20Tecnol%C3%B3gico%2C%20San%20Luis%20Potos%C3%AD%20S.L.P',
+    },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://facebook.com/' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://instagram.com/' },
+    { ariaLabel: 'YouTube', icon: 'tabler:brand-youtube', href: 'https://youtube.com/' },
   ],
 
-  // Iconos inferiores
-socialLinks: [
-  {
-    ariaLabel: 'Email',
-    icon: 'tabler:mail',
-    href: 'https://mail.google.com/mail/?view=cm&fs=1&to=emmaidaliasaldana@gmail.com',
-  },
-  { ariaLabel: 'WhatsApp', icon: 'tabler:brand-whatsapp', href: 'https://wa.me/524441425726' },
-  {
-    ariaLabel: 'Mapa',
-    icon: 'tabler:map-pin',
-    href: 'https://www.google.com/maps/search/?api=1&query=Ca%C3%B1ada%20Real%20316%2C%20C.P.%2078215%2C%20Lomas%20del%20Tecnol%C3%B3gico%2C%20San%20Luis%20Potos%C3%AD%20S.L.P',
-  },
-  { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://facebook.com/' },
-  { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://instagram.com/' },
-  { ariaLabel: 'YouTube', icon: 'tabler:brand-youtube', href: 'https://youtube.com/' },
-],
-
-  footNote: `
-    © ${new Date().getFullYear()} SOOMA Consultores. Todos los derechos reservados.
-  `,
+    footNote: `
+      © ${new Date().getFullYear()} SOOMA Consultores. Todos los derechos reservados.
+    `,
 };
